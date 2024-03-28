a96115e301961e0730a7590b34f07590f1d86187
Ayakhinm <rayanhilliman@egmail.com>
2024-03-28 15:21:54 +0100

    Correctif du premier spider

--- spiders/mon_spider.py
+++ spiders/mon_spider.py
@@ -1,12 +1,16 @@
 import scrapy
-
+import time
 
 class MonSpiderSpider(scrapy.Spider):
     name = "mon_spider"
     allowed_domains = ["www.hellowork.com"]
     start_urls = ["https://www.hellowork.com/fr-fr/"]
+    custom_settings = {
+        'ROBOTSTXT_OBEY': False,
+        'DOWNLOAD_DELAY': 20  # Ajoutez un délai de 3 secondes entre chaque requête
+    }
 
     def parse(self, response):
         extracted_text = response.css('h1.tw-inline-flex span::text').getall()
-        extracted_text = ' '.join(extracted_text).strip()
+        extracted_text = ' '.join(extracted_text).strip().replace('\u202f', ' ')
         yield {'extracted_text': extracted_text}
